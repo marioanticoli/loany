863216e0a201b99f058a73a7b3a044c2c9c114be
Mario Anticoli <mario@mario.local>
2019-10-07 19:25:34 +0200

    validation loan application

--- lib/loany/loans/application.ex
+++ lib/loany/loans/application.ex
@@ -17,5 +17,14 @@ defmodule Loany.Loans.Application do
     application
     |> cast(attrs, [:loan_amount, :first_name, :last_name, :phone, :email])
     |> validate_required([:loan_amount, :first_name, :last_name, :phone, :email])
+    |> validate_number(:loan_amount, greater_than: 0)
+    |> validate_length(:first_name, min: 1)
+    |> validate_length(:last_name, min: 1)
+    |> validate_format(:phone, ~r/\+[0-9]{6,11}/,
+      message: "Please enter your number starting with a + followed by only numbers"
+    )
+    |> validate_format(:email, ~r/[^@]+@[^\.]+\..+/,
+      message: "Please enter your email like username@domain.tld"
+    )
   end
 end
