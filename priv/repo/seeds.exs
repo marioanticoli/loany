17a6182887afa49783684beee6326fc5f92aaf19
Mario Anticoli <mario@mario.local>
2019-10-08 02:54:37 +0200

    schema+migration+seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,8 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+Loany.Repo.insert!(%Loany.Users.User{
+  email: "admin@example.com",
+  password_hash: Pow.Ecto.Schema.Password.pbkdf2_hash("admin")
+})
